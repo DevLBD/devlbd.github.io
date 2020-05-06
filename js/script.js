0ffa4f59a5e7f13f56cb6cf49ac1caa8b41329ae
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2020-05-06 08:13:16 +0200

    Introducing new Portfolio Design, Removing useless resources, and more.
    This update releases a new design to my portfolio and removes some unnecessary things.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-// Just a little joke. :)
-var portfolioButton = document.getElementById("portfolio");
-portfolioButton.addEventListener("click", trollingUser);
-function trollingUser() {
-    alert("You're already here, lol.")
-}
+var lastScrollTop = 0;
+
+$(window).scroll(function(){
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+        // Downscroll code.
+        console.log("Scrolled Down.");
+        $(".navbar").addClass("scrolled");
+        $(".title").addClass("scrolled");
+        $(".night").addClass("scrolled");
+        $(".darkbt").addClass("scrolled");
+        $(".menu").addClass("scrolled");
+        $(".menubt").addClass("scrolled");
+        $(".scroll-downs").addClass("scrolled");
+    } else {
+        // Upscroll code
+        console.log("Scrolled Up.");
+        $(".navbar").removeClass("scrolled");
+        $(".title").removeClass("scrolled");
+        $(".night").removeClass("scrolled");
+        $(".darkbt").removeClass("scrolled");
+        $(".menu").removeClass("scrolled");
+        $(".menubt").removeClass("scrolled");
+        $(".scroll-downs").removeClass("scrolled");
+    }
+    lastScrollTop = st;
+});
