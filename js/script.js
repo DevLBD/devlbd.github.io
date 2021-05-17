c7b421c6549741f940eea525fc055f7f8e09a86b
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-05-17 19:34:21 +0200

    Starting to redesign my Portfolio.
    This version introduces a redesign which will be completed around the end of the summer.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,15 @@
-var lastScrollTop = 0;
-
-$(window).scroll(function(){
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop){
-        // Downscroll code.
-        console.log("Scrolled Down.");
-        $(".navbar").addClass("scrolled");
-        $(".title").addClass("scrolled");
-        $(".night").addClass("scrolled");
-        $(".darkbt").addClass("scrolled");
-        $(".menu").addClass("scrolled");
-        $(".menubt").addClass("scrolled");
-        $(".scroll-downs").addClass("scrolled");
-    } else {
-        // Upscroll code
-        console.log("Scrolled Up.");
-        $(".navbar").removeClass("scrolled");
-        $(".title").removeClass("scrolled");
-        $(".night").removeClass("scrolled");
-        $(".darkbt").removeClass("scrolled");
-        $(".menu").removeClass("scrolled");
-        $(".menubt").removeClass("scrolled");
-        $(".scroll-downs").removeClass("scrolled");
-    }
-    lastScrollTop = st;
+$("document").ready(function(){
+    $("button.first").hover(function(){
+        $(".rotated").addClass("moveDown");
+    }, function(){
+        $(".rotated").removeClass("moveDown");
+    });
 });
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY==0){
+      $("nav").removeClass("scrolled");
+    } else {
+      $("nav").addClass("scrolled");
+    }
+  });
